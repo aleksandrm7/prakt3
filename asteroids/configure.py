ba9e30e2ee1e6983da491daa5fe2550fbde1b9cb
aleksandrm7 <80441149+aleksandrm7@users.noreply.github.com>
2023-01-14 01:17:59 +0300

    Create configure.py

---
+++ asteroids/configure.py
@@ -0,0 +1,15 @@
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+
+WIDTH = 800
+HEIGHT = 600
+score_start = 0
+lives_start = 3 
+started = False 
+ang_vel = 4
+
+time = 0
+score = score_start
+lives = lives_start
